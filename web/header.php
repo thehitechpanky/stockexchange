f7afb236a542ed55af18e16243a172ec2c91317e
Pankaj Jain <capankajjain@smilyo.com>
2015-08-03 22:00:46 +0530

    Create header.php

---
+++ web/header.php
@@ -0,0 +1,33 @@
+<?php //include "./dbConfig.php"; ?>
+<!--A Design by W3layouts
+Author: W3layout
+Author URL: http://w3layouts.com
+License: Creative Commons Attribution 3.0 Unported
+License URL: http://creativecommons.org/licenses/by/3.0/
+-->
+<!DOCTYPE HTML>
+<html>
+<!-- Head Starts -->
+<head>
+<title>Free Ascend E-commerce Website Template | Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link href="web/css/style.css" rel="stylesheet" type="text/css" media="all"/>
+<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,700,500,900' rel='stylesheet' type='text/css'>
+	<link href="./css/minifiedCss.php" rel="stylesheet" type="text/css" >
+	<script src="web/js/jquery.min.js" type="text/javascript"></script>
+	<script src="web/js/script.js" type="text/javascript"></script>
+	<!-- Start Custom Scripts -->
+	<script src="./js/fb.js" type="text/javascript"></script>
+	<!-- End Custom Scripts -->
+<script src="web/js/jquery.magnific-popup.js" type="text/javascript"></script>
+<link href="web/css/magnific-popup.css" rel="stylesheet" type="text/css">
+<!-- Add fancyBox main JS and CSS files -->
+	<script type="text/javascript" src="web/js/jquery.fancybox.js"></script>
+	<link rel="stylesheet" type="text/css" href="web/css/jquery.fancybox.css" media="screen" />
+	<script type="text/javascript" src="web/js/jquery.fancybox-media.js"></script>
+</head>
+<!-- Head Ends -->
+
+<!-- Body Starts -->
+<body>
