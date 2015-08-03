3ba50bfd4ec74c3e133a876a94acfb367cfe1711
Pankaj Jain <capankajjain@smilyo.com>
2015-08-04 02:23:57 +0530

    Create buySell.js

---
+++ js/buySell.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$('.popup-with-zoom-anim').magnificPopup({
+		type: 'inline',
+		
+		fixedContentPos: false,
+		fixedBgPos: true,
+		
+		overflowY: 'auto',
+		
+		closeBtnInside: true,
+		preloader: false,
+		
+		midClick: true,
+		removalDelay: 300,
+		mainClass: 'my-mfp-zoom-in'
+	});
+	
+	$('#shareId').change(function() {
+		showQtyAvailable(this.value);
+		showMarketPrice(this.value);
+	});
+});
+
+function showMarketPrice(str) {
+    if (str > 0) {
+        if (window.XMLHttpRequest) {
+            // code for IE7+, Firefox, Chrome, Opera, Safari
+            xmlhttpMarketPrice = new XMLHttpRequest();
+        } else {
+            // code for IE6, IE5
+            xmlhttpMarketPrice = new ActiveXObject("Microsoft.XMLHTTP");
+        }
+        xmlhttpMarketPrice.onreadystatechange = function() {
+            if (xmlhttpMarketPrice.readyState == 4 && xmlhttpMarketPrice.status == 200) {
+                document.getElementById("marketPrice").innerHTML = xmlhttpMarketPrice.responseText;
+            }
+        }
+        xmlhttpMarketPrice.open("GET","getMarketPrice.php?shareId="+str,true);
+        xmlhttpMarketPrice.send();
+    } else { 
+       document.getElementById("marketPrice").innerHTML = "";
+       return; 
+    }
+}
