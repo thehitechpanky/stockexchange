6815383167db60ceaf00d9d638b6fc1a3f6a569a
Pankaj Jain <capankajjain@smilyo.com>
2015-08-04 10:28:27 +0530

    Update buysell.php

--- web/buysell.php
+++ web/buysell.php
@@ -1,18 +1,23 @@
 <div id="small-dialog" class="mfp-hide sign_up">
 	<h1>Trading Window</h1>
 	<form width="100%">
-		What would you like to do?
-		<input type="radio" name="order" value="buy" id="orderBuy" checked>Buy
-		<input type="radio" name="order" value="sell" id="orderSell">Sell<br />
 		Market
 		<input type="radio" name="market" value="primary" id="marketPrimary">Primary
 		<input type="radio" name="market" value="secondary" id="marketSecondary" checked>Secondary<br />
+		Order Type
+		<input type="radio" name="order" value="buy" id="buyOrder" checked>Buy
+		<input type="radio" name="order" value="sell" id="sellOrder">Sell
+		<input type="radio" name="order" value="bid" id="bidOrder" disabled>Bid
+		<br />
 		<select>
 			<option value="" disabled selected>Category</option>
 			<option value="celebrity">Celebrity</option>
 			<option value="college">College</option>
 		</select><br />
-		<input id="shareId" type="text" placeholder="Share" /><br />
+		<input id="shareName" type="text" placeholder="Share Name" list="shareList" />
+		<datalist id="shareList">
+		</datalist>
+		<input id="shareId" type="hidden" placeholder="" disabled />
 		<div id="qtyAvailable"></div>
 		<div id="marketPrice"></div>
 		<input type="text" placeholder="Qty to Trade" /><br />
