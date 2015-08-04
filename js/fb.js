aa9eb2ebae4365fc802277bb82e3764270c05316
Pankaj Jain <capankajjain@smilyo.com>
2015-08-04 10:30:02 +0530

    Update fb.js

--- js/fb.js
+++ js/fb.js
@@ -187,25 +187,3 @@ function showQtyAvailable(str) {
 		return;
 	}
 }
-
-function showMarketPrice(str) {
-    if (str == "") {
-        document.getElementById("marketPrice").innerHTML = "";
-        return;
-    } else { 
-        if (window.XMLHttpRequest) {
-            // code for IE7+, Firefox, Chrome, Opera, Safari
-            xmlhttpMarketPrice = new XMLHttpRequest();
-        } else {
-            // code for IE6, IE5
-            xmlhttpMarketPrice = new ActiveXObject("Microsoft.XMLHTTP");
-        }
-        xmlhttpPortfolio.onreadystatechange = function() {
-            if (xmlhttpMarketPrice.readyState == 4 && xmlhttpMarketPrice.status == 200) {
-                document.getElementById("marketPrice").innerHTML = xmlhttpMarketPrice.responseText;
-            }
-        }
-        xmlhttpMarketPrice.open("GET","getMarketPrice.php?shareId="+str,true);
-        xmlhttpMarketPrice.send();
-    }
-}
